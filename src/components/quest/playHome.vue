cd23cba16f47e6ee65b5a3c5eb876150fb62bd8f
Maxime Pacary <maxime.pacary@gmail.com>
2018-02-20 11:16:35 +0100

    quest home: showing picture & author if available

--- src/components/quest/playHome.vue
+++ src/components/quest/playHome.vue
@@ -2,13 +2,13 @@
   
   <div class="wrapper">
     <div class="row bottom-separator">
-      <div class="col-3 padding-medium">
+      <div class="col-3 padding-medium" v-if="typeof quest.picture !== 'undefined'">
         <img :src="serverUrl + '/upload/quest/' + quest.picture" class="full-width" />
       </div>
       
       <div class="col desc padding-medium">
         <h1>{{ quest.title }}</h1>
-        <p class="text-tertiary">[[Auteur]]</p>
+        <p class="text-tertiary" v-if="typeof quest.author !== 'undefined'">{{ quest.author.name }}</p>
         <p v-if="quest.rating">
           <q-rating readonly :value="Math.round(quest.rating)" color="primary" :max="5" size="1.7rem" />
         </p>
