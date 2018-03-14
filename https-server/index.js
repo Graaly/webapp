9b659a0e1d1a31c0f58b517a9e7d95843e674208
Eric Mathieu <eric.mathieu@kimind.com>
2018-03-14 09:26:59 +0100

    favicons

--- https-server/index.js
+++ https-server/index.js
@@ -1,10 +1,49 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  port = process.env.PORT || 80
+var express  = require('express');
+var https    = require('https');
+var fs       = require('fs');
+var app      = express();
 
-const app = express()
+var favicon      = require('serve-favicon');
+// var basicAuth    = require('basic-auth-connect');
+// var cookieParser = require('cookie-parser');
+// var bodyParser   = require('body-parser');
 
-app.use(serveStatic('/var/www/prod/webapp/dist/'))
-app.listen(port)
+var server = app;
+
+// app.use(basicAuth('kimind', 'cix4ever'));
+// app.use(cookieParser()); // read cookies (needed for auth)
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+//redirect http > https
+app.use(function(req, res, next) {
+  if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  }
+  else
+    next();
+});
+
+//redirect www > non www
+app.get('/*', function(req, res, next) {
+  if (req.headers.host.match(/^www/) !== null ) {
+    res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+  } else {
+    next();
+  }
+})
+// robots.txt
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.send("User-agent: *\nDisallow: /");
+});
+
+app.use(express.static('/var/www/prod/webapp/dist/'));
+app.use(favicon('/var/www/prod/webapp/https-server/favicon.ico'));
+
+// =================================================================
+// start the server ================================================
+// =================================================================
+var port = 443;
+server.listen(port);
 console.log('Server successfully started on PRODUCTION on port ' + port);
