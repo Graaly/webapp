249f737662fe202a69ad2f5a28ff56f1a10fc0f4
Eric Mathieu <eric.mathieu@kimind.com>
2018-03-16 23:18:37 +0100

    change server location on prod

--- src/services/Api.js
+++ src/services/Api.js
@@ -6,10 +6,12 @@ axios.defaults.withCredentials = true
 // Note: HTTPS is mandatory here because HTTPS is required for front (geolocation)
 
 export default() => {
+  // temp hack
+  var baseUrl = (window.location.hostname.indexOf("graaly.com") == -1 ? 'https://' + window.location.hostname + ':3000' : 'https://api.graaly.com')
   return axios.create({
     // currently for proto, API will be always available from same host as webapp
     // => localhost, 78.247.66.31, etc.
-    baseURL: 'https://' + window.location.hostname + ':3000',
+    baseURL: baseUrl,
     validateStatus: function (status) {
       return status < 500 //let app treat 3xx and 4xx errors
     }
