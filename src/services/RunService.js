a9bfe59a203726b8461cb99545bbb887e48d61a5
Maxime Pacary <maxime.pacary@gmail.com>
2018-09-11 16:25:09 +0200

    fixed issue when playing quest & not being quest author : it was not possible to skip "skippable" steps (geoloc, puzzle...), player was redirected to the previous step instead of the next. + cleaned old "quest item icons", not used anymore.

--- src/services/RunService.js
+++ src/services/RunService.js
@@ -3,28 +3,28 @@ import Api from 'services/Api'
 export default {
   /*
    * get run based on its ID (only for connected user)
-   * @param   {String}    id                  ID f the run
+   * @param   {String}    id                  ID of the run
    */
   getById (id) {
     return Api().get('run/' + id)
   },
   /*
    * list runs for a quest and a user
-   * @param   {String}    questId                  ID f the quest
+   * @param   {String}    questId                  ID of the quest
    */
   listForAQuest (questId) {
     return Api().get('run/quest/' + questId)
   },
   /*
    * list players that played this quest
-   * @param   {String}    questId                  ID f the quest
+   * @param   {String}    questId                  ID of the quest
    */
   listPlayersForThisQuest (questId) {
     return Api().get('run/quest/' + questId + '/ranking')
   },
   /*
    * get a step hint
-   * @param   {String}    runId                 ID f the run
+   * @param   {String}    runId                 ID of the run
    * @param   {String}    stepId                ID of the step
    */
   async getHint (runId, stepId) {
@@ -33,7 +33,7 @@ export default {
   },
   /*
    * init a new run for current user for a quest
-   * @param   {String}    questId             ID f the quest
+   * @param   {String}    questId             ID of the quest
    * @param   {String}    lang                Language concerned
    * @param   {Boolean}   remotePlay          is the player playing remotely
    */
@@ -42,17 +42,24 @@ export default {
   },
   /*
    * set a run as finished
-   * @param   {String}    id                  ID f the run
+   * @param   {String}    id                  ID of the run
    */
   endRun (id) {
     return Api().put('run/' + id + '/end')
   },
   /*
    * rate a run & a quest
-   * @param   {String}    id                  ID f the run
+   * @param   {String}    id                  ID of the run
    * @param   {String}    rating              Rating (1 > 5)
    */
   rate (id, rating) {
     return Api().put('run/' + id + '/rate/' + rating)
+  },
+  /*
+   * skip a step
+   * @param   {String}    id                  ID of the run
+   */
+  passCurrentStep(id) {
+    return Api().put('run/' + id + '/passCurrentStep')
   }
 }
