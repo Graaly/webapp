bad3d02663c1b8267781ef4643f8dddd496b444e
mpacary <maxime.pacary@free.fr>
2019-10-24 15:42:43 +0200

    JWT and WKWebView on iOS

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: 'production',
-  SERVER_URL: 'https://api.graaly.com',
+  //SERVER_URL: 'https://api.graaly.com',
+  SERVER_URL: 'https://192.168.0.10:3000',
   PERSIST_STATE_COOKIE_DURATION: 30 // days
 }
