a7db39069de1a03b034377c913718bc951a572be
Eric Mathieu <eric.mathieu@kimind.com>
2019-12-15 23:45:16 +0100

    change required version

--- src/boot/RouterAuthentication.js
+++ src/boot/RouterAuthentication.js
@@ -43,7 +43,7 @@ export default ({ app, router, Vue }) => {
           Loading.hide()
 
           if (response && response.data && response.data.name) {
-            if (response.data.clientSupportedVersion && response.data.clientSupportedVersion > "1.7.0") {
+            if (response.data.clientSupportedVersion && response.data.clientSupportedVersion > "1.8.1") {
               next({
                 path: '/error/upgraderequired'
               })
