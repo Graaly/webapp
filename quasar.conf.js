c504a87d6c27a357ea7f534c3cef4a350b9f4d37
Adrien RICHARD <richardadrien0@gmail.com>
2020-07-28 14:47:36 +0200

    Sentry error monitoring
    https://sentry.io

--- quasar.conf.js
+++ quasar.conf.js
@@ -8,6 +8,7 @@ module.exports = function (ctx) {
   return {
     // app boot (/src/boot)
     boot: [
+      'SentryMonitoring',
       'FirebaseMonitoring',
       'CustomDirectives',
       'DateFormatFilter',
