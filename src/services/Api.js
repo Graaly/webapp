003dee5711111ce4e8143b63e0710beba61070cb
Maxime Pacary <maxime.pacary@free.fr>
2021-02-17 14:19:40 +0100

    improved handling of unstable networks

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-import store from "../store/index"
 import axiosRetry from 'axios-retry'
 
 // Note: HTTPS is mandatory here because HTTPS is required for front (geolocation)
@@ -13,39 +12,18 @@ let myAxios = axios.create({
   }
 });
 
-myAxios.defaults.timeout = 4500;
-myAxios.defaults.headers.common["Authorization"] = `Bearer ${
-  localStorage["jwt"]
-}`;
+myAxios.defaults.timeout = 4500 // slightly before axiosRetry delay
+myAxios.defaults.headers.common["Authorization"] = `Bearer ${localStorage["jwt"]}`
 
 axiosRetry(myAxios, {
-  retries: 100,
-  /*retryCondition: (err) => {
-    console.log('*retryCondition!*', JSON.stringify(err, null, 2), 'attempt#' + err.config['axios-retry'].retryCount, err.code)
-    if (!err.response) {
-      store.dispatch('setNetworkStatus', false)
-    }
-    return !err.response; // same as default
-  },*/
+  retries: 10,
+  retryCondition: (err) => {
+    return err.code && err.code === 'ECONNABORTED' // any other error than timeout: no retry
+  },
   shouldResetTimeout: true,
   retryDelay: () => { return 5000 }
 })
 
-// intercept timeouts
-/*myAxios.interceptors.response.use(
-  (res) => {
-    //console.log('response success intercepted')
-    //console.log('WE CAN CONSIDER THAT WE ARE ONLINE')
-    store.dispatch('setNetworkStatus', true)
-    return res
-  },
-  (err) => {
-    //console.log('response error intercepted', err)
-    if (err.code === 'ECONNABORTED') {
-      //console.log('*** TIMEOUT DETECTED: WE CAN CONSIDER THAT WE ARE OFFLINE !')
-      store.dispatch('setNetworkStatus', false)
-    }
-  }
-)*/
-
-export default () => { return myAxios }
+export default () => {
+  return myAxios
+}
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,6 @@ import * as Cookies from "js-cookie";
 
 import AuthService from "services/AuthService";
 
-import utils from "../includes/utils"
-
 Vue.use(Vuex);
 
 // root state object.
@@ -28,12 +26,7 @@ const state = {
   // for quest steps, keep track of objects given by setInterval() & setTimeout() to clear them properly & avoid useless calls
   runningIntervalIds: [],
   runningTimeoutsIds: [],
-  mediaStreams: [],
-  isNetworkStable: null,
-  networkCheckDelay: {
-    initial: 5, // in seconds
-    current: null
-  }
+  mediaStreams: []
 };
 
 // mutations are operations that actually mutates the state.
@@ -94,14 +87,6 @@ const mutations = {
       stream.getTracks().forEach(track => track.stop());
     }
     state.mediaStreams = [];
-  },
-  setNetworkStatus(state, status) {
-    //console.log("*mutate setNetworkStatus*", status)
-    state.isNetworkStable = Boolean(status)
-  },
-  setNetworkCheckDelay(state, delay) {
-    //console.log("*mutate setNetworkCheckDelay*", delay)
-    state.networkCheckDelay.current = delay
   }
 }
 
@@ -150,46 +135,7 @@ const actions = {
   addIntervalId: ({ commit }, intervalId) => commit("addIntervalId", intervalId),
   clearIntervalIds: ({ commit }) => commit("clearIntervalIds"),
   addMediaStream: ({ commit }, stream) => commit("addMediaStream", stream),
-  clearMediaStreams: ({ commit }) => commit("clearMediaStreams"),
-  setNetworkStatus: ({ state, commit, dispatch }, status) => {
-    //console.log('* action setNetworkStatus *', status, state.isNetworkStable)
-    if (Boolean(status) !== state.isNetworkStable) {
-      //console.log('*network status change detected!*')
-      commit("setNetworkCheckDelay", null)
-      //TEMP
-      //dispatch("checkNetworkPeriodically")
-    }
-    commit("setNetworkStatus", status)
-  },
-  setNetworkCheckDelay: ({ commit }, delay) => commit("setNetworkCheckDelay", delay),
-  /**
-   * checks periodically network quality
-   * insipired from https://dev.to/oskarcodes/quick-javascript-tip-setinterval-with-modifiable-delay-4h1g and https://github.com/HubSpot/offline
-   */
-  checkNetworkPeriodically: async ({ state, commit, dispatch }) => {
-    let delay = state.networkCheckDelay
-    let newCurrentDelay = delay.current
-    
-    // this will reset automatically network status & timeout
-    // in case of network status change:
-    // - failed call due to timeout when network status is defined as stable
-    // - successful call when network status is defined as unstable
-    
-    // TEMPORARILY DISABLED FOR TESTING AXIOS RETRY
-    //await NetworkService.basicApiCall()
-    
-    if (delay.current === null || !state.isNetworkStable) {
-      newCurrentDelay = delay.initial
-    } else {
-      // when network is stable, increase the delay between each call, capped at 5 minutes (in seconds)
-      newCurrentDelay = Math.min(delay.current * 1.5, 300)
-    }
-    commit("setNetworkCheckDelay", newCurrentDelay)
-    //console.log('NETWORK WILL BE CHECKED IN ' + newCurrentDelay + ' SECONDS')
-    await utils.sleep(newCurrentDelay * 1000)
-    //TEMP
-    //dispatch("checkNetworkPeriodically")
-  }
+  clearMediaStreams: ({ commit }) => commit("clearMediaStreams")
 };
 
 // getters are functions
