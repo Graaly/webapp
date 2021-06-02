16ae95e3300ba01863ed7c5ea01f40d46ff88595
Adrien RICHARD <richardadrien0@gmail.com>
2021-06-02 15:28:28 +0200

    Changed Api adress

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   async getUserMessages(userId) {
     return axios
-      .get(`http://localhost:3000/messages/${userId}`)
+      .get(`http://gmdash.api-graaly.com:3000:3000/messages/${userId}`)
       .then(response => response.data)
       .catch(error => error)
   }
