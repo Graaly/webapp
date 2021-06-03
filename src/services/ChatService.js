4b1577230d7ad2e71012d2934023480b2fe227ba
Adrien RICHARD <richardadrien0@gmail.com>
2021-06-03 16:50:58 +0200

    fixed http adress

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   async getUserMessages(userId) {
     return axios
-      .get(`http://gmdash.api-graaly.com:3000:3000/messages/${userId}`)
+      .get(`https://gmdash.api-graaly.com:3001/messages/${userId}`)
       .then(response => response.data)
       .catch(error => error)
   }
