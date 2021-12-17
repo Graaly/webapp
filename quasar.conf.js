17aa245d4aa87353d475d53ae12437d55d32e725
Eric Mathieu <eric.mathieu13@gmail.com>
2021-12-17 23:40:22 +0100

    update versions

--- quasar.conf.js
+++ quasar.conf.js
@@ -195,7 +195,7 @@ module.exports = function (ctx) {
       }
     },
     cordova: {
-      version: "2.1.43"
+      version: "2.1.44"
       // id: 'org.cordova.quasar.app'
     },
     electron: {
