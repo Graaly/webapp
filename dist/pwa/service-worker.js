89f85a1c13cfc57f77b661e3eac4c7eb21e6c014
Eric Mathieu <eric.mathieu13@gmail.com>
2021-12-18 17:59:08 +0100

    new pwa version

--- dist/pwa/service-worker.js
+++ dist/pwa/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.99ea398be08fdabb1578b425a63aa08c.js"
+  "precache-manifest.82261d6783568c2b13e089fff90f2d4c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "graaly-frontend"});
