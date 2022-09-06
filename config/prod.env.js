367b056ae7797b3cbf3fd7a5a0efc45e2335dae1
Eric Mathieu <eric.mathieu13@gmail.com>
2022-09-06 21:33:39 +0200

    change prod settings

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: 'production',
   SERVER_URL: 'https://api.graaly.com', // prod
+  UPLOAD_URL: 'https://graaly-upload.s3.eu-west-3.amazonaws.com',
   //SERVER_URL: 'https://192.168.1.39:3000',
   //SERVER_URL: 'https://graaly.duckdns.org:38512', // preprod
   MQTT_URL: 'wss://api.graaly.com:9001', // to be confirmed
