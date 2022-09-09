edf197fcc19c2a6ff6b8f3a01ab73d450d66cc4e
BARTOSNicolas <nicolas.bartos@le-campus-numerique.fr>
2022-09-09 13:16:02 +0200

    version update

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,5 +8,5 @@ module.exports = {
   MQTT_URL: 'wss://api.graaly.com:9001', // to be confirmed
   MQTT_TOPIC: 'Graaly',
   PERSIST_STATE_COOKIE_DURATION: 30, // days,
-  VERSION: "2.2.0"
+  VERSION: "2.2.1"
 }
